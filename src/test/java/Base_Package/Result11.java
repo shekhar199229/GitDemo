e6b03591a692c76c36dc88eab181f5198baa39a3
Shekhar <umshettyshekhar@gmail.com>
2020-05-11 18:40:22 +0530

    First message

---
+++ src/test/java/Base_Package/Result11.java
@@ -0,0 +1,18 @@
+package Base_Package;
+
+import org.testng.annotations.Test;
+
+public class Result11 {
+
+	@Test
+	public void Login() {
+		
+		System.out.println("Hello Login");
+	}
+
+	@Test
+	public void MyRLoginResult() {
+		
+		System.out.println("Hey LoginResult");
+	}
+}
---
+++ src/test/java/Base_Package/Test11.java
@@ -0,0 +1,19 @@
+package Base_Package;
+
+import org.testng.annotations.Test;
+
+public class Test11 {
+	
+@Test	
+public void Mytest() {
+	
+	System.out.println("Hello People");
+}
+
+@Test
+public void MyResult1() {
+	
+	System.out.println("Hey result");
+}
+
+}
